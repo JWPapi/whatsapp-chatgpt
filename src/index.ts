045a7cd3b1b0353ccf131c741d44d64aba263739
JWPapi <juliantosun@gmail.com>
2024-09-17 13:14:28 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -77,7 +77,6 @@ const start = async () => {
 		// Ignore if message is from status broadcast
 		if (message.from == constants.statusBroadcast) return;
 
-		// Ignore if it's a quoted message, (e.g. Bot reply)
 
 		await handleIncomingMessage(message);
 	});
@@ -88,10 +87,8 @@ const start = async () => {
 		if (message.from == constants.statusBroadcast) return;
 
 		// Ignore if it's a quoted message, (e.g. Bot reply)
-		if (message.hasQuotedMsg) return;
 
 		// Ignore if it's not from me
-		if (!message.fromMe) return;
 
 		await handleIncomingMessage(message);
 	});
