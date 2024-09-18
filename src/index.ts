dfd0cd6498421e50fecff3acaf89cf8177b44ebd
JWPapi <juliantosun@gmail.com>
2024-09-18 16:52:40 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -87,8 +87,10 @@ const start = async () => {
 		if (message.from == constants.statusBroadcast) return;
 
 		// Ignore if it's a quoted message, (e.g. Bot reply)
+		if (message.hasQuotedMsg) return;
 
 		// Ignore if it's not from me
+		if (!message.fromMe) return;
 
 		await handleIncomingMessage(message);
 	});
