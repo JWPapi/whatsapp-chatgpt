72fd3f9efa99098b62aced86d3cf6c316cc8603f
jwpapi <juliantosun@gmail.com>
2025-06-20 15:52:55 +0200

    Update

--- src/cli/ui.js
+++ src/cli/ui.js
@@ -1,39 +1,43 @@
-import { intro, spinner, note, outro, text } from "@clack/prompts";
-import color from "picocolors";
+import { intro, note, outro, spinner } from '@clack/prompts'
+import color from 'picocolors'
 
-const s = spinner();
+const s = spinner()
 
-export const print = (text) => {
-	console.log(color.green("◇") + "  " + text);
-};
+export const print = text => {
+  console.log(color.green('◇') + '  ' + text)
+}
 
 export const printIntro = () => {
-	intro(color.bgCyan(color.white(" Whatsapp ChatGPT Bot "))); // Removed DALL-E
-	note("A Whatsapp bot that uses OpenAI's ChatGPT to generate text from a prompt."); // Removed DALL-E
-	s.start("Starting");
-};
+  intro(color.bgCyan(color.white(' Whatsapp ChatGPT Bot '))) // Removed DALL-E
+  note("A Whatsapp bot that uses OpenAI's ChatGPT to generate text from a prompt.") // Removed DALL-E
+  s.start('Starting')
+}
 
-export const printQRCode = (qr) => {
-	// Removed : string type
-	s.stop("Client is ready!");
-	note(qr, "Scan the QR code below to login to Whatsapp Web.");
-};
+export const printQRCode = qr => {
+  // Removed : string type
+  s.stop('Client is ready!')
+  note(qr, 'Scan the QR code below to login to Whatsapp Web.')
+  note(qr)
+  note(qr)
+  note(qr)
+  note(qr)
+}
 
 export const printLoading = () => {
-	s.stop("Authenticated!");
-	s.start("Logging in");
-};
+  s.stop('Authenticated!')
+  s.start('Logging in')
+}
 
 export const printAuthenticated = () => {
-	s.stop("Session started!");
-	s.start("Opening session");
-};
+  s.stop('Session started!')
+  s.start('Opening session')
+}
 
 export const printAuthenticationFailure = () => {
-	s.stop("Authentication failed!");
-};
+  s.stop('Authentication failed!')
+}
 
 export const printOutro = () => {
-	s.stop("Loaded!");
-	outro("Whatsapp ChatGPT Bot is ready to use."); // Removed DALL-E
-};
+  s.stop('Loaded!')
+  outro('Whatsapp ChatGPT Bot is ready to use.') // Removed DALL-E
+}
