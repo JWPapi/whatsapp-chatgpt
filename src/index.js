f5485826bf2435f8ec8aa69fb35315f5a13c789c
Your Name <you@example.com>
2025-07-27 11:32:42 +0200

    chore: add environment logging for debugging purposes

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import { setupCronJobs } from './cron/cron.js'
 
 let botReadyTimestamp = null
 
+console.log('environment:', process.env.ENVIRONMENT)
+
 // Initialize client outside the start function to make it accessible to the signal handler
 const client = new Client({
   puppeteer: {
